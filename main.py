4a0bbfe54871d93a42773d277cbc17422ed89891
thejduman <thejduman@yahoo.com>
2020-07-14 16:51:42 +0000

    Made a simple print program

---
+++ main.py
@@ -0,0 +1,7 @@
+name = "Julian"
+artist = "Seven Lions"
+vacation = "Spain"
+hobby = "gaming"
+date = "7/14/20"
+
+print("Hello, my name is " + name + ". My favorite artist is " + artist + ". One place I would like to go on a vacation to is "+ vacation + ". One of my hobbies is " + hobby + ". Today's date is " + date + ".")
